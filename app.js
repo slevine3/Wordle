ad9a2f235120788acb6f462df2a6dd19d02e286f
slevine3 <levinesam3@gmail.com>
2022-03-23 18:20:03 -0700

    functions created

--- app.js
+++ app.js
@@ -15290,3 +15290,37 @@ const dictionary = [
   "shave",
 ];
 
+const startInteraction = () => {
+  document.addEventListener("click", handleMouseClick);
+  document.addEventListener("keydown", handleKeyPress);
+};
+
+const handleMouseClick = (e) => {
+  if (e.target.matches("[data-key")) {
+    pressKey(e.target.dataset.key);
+    return;
+  }
+  if (e.target.matches(["[data-enter]"])) {
+    submitGuess();
+    return;
+  }
+  if (e.target.matches("[data-delete]")) {
+    deleteKey();
+    return;
+  }
+};
+
+const handleKeyPress = (e) => {
+  if (e.key === "Enter") {
+    submitGuess();
+    return;
+  }
+  if (e.key === "Backspace" || e.key == "Delete") {
+    deleteKey();
+    return;
+  }
+  if (e.key.match(/^[a-z]$/)){
+      pressKey(e.key)
+      return;
+  }
+};
