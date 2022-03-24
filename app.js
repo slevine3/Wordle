d8231e83957c7994bb94b025281c849c4e8da01f
slevine3 <levinesam3@gmail.com>
2022-03-23 18:43:40 -0700

    add and remove letters

--- app.js
+++ app.js
@@ -15290,9 +15290,21 @@ const dictionary = [
   "shave",
 ];
 
+const WORD_LENGTH = 5;
+
+const guessGrid = document.querySelector("[data-guess-grid]");
+const targetWord = "";
+
+
+
 const startInteraction = () => {
   document.addEventListener("click", handleMouseClick);
   document.addEventListener("keydown", handleKeyPress);
+};
+
+const stopInteraction = () => {
+  document.removeEventListener("click", handleMouseClick);
+  document.removeEventListener("keydown", handleKeyPress);
 };
 
 const handleMouseClick = (e) => {
@@ -15319,8 +15331,38 @@ const handleKeyPress = (e) => {
     deleteKey();
     return;
   }
-  if (e.key.match(/^[a-z]$/)){
-      pressKey(e.key)
-      return;
+  if (e.key.match(/^[a-z]$/)) {
+    pressKey(e.key);
+    return;
   }
 };
+
+const pressKey = (key) => {
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length >= WORD_LENGTH) return;
+
+  const nextTile = guessGrid.querySelector(":not([data-letter]");
+  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.textContent = key;
+  nextTile.dataset.state = "active";
+};
+
+const deleteKey = () => {
+  const activetiles = getActiveTiles();
+  const lastTile = activeTiles[activeTiles.length - 1];
+
+  if (lastTile == null) return;
+  lastTile.textContent = "";
+  delete lastTile.dataset.state;
+  delete lastTile.dataset.letter;
+};
+
+const submitGuess = () => {
+    
+}
+
+const getActiveTiles = () => {
+  return guessGrid.querySelectorAll('[data-state="active"]');
+};
+
+startInteraction();
